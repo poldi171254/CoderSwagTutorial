c3d62033e990c93bc8749138fb8712b266f0b0b3
Poldi <leo@zudiewiener.com>
2021-02-12 14:46:32 +1100

    Implement RecycleView

--- app/src/main/java/com/zudiewiener/coderswag2/Controler/MainActivity.kt
+++ app/src/main/java/com/zudiewiener/coderswag2/Controler/MainActivity.kt
@@ -3,7 +3,10 @@ package com.zudiewiener.coderswag2.Controler
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.zudiewiener.coderswag2.Adapters.CategoryAdapter
+import com.zudiewiener.coderswag2.Adapters.CategoryRecycleAdapter
 import com.zudiewiener.coderswag2.Model.Category
 import com.zudiewiener.coderswag2.R
 import com.zudiewiener.coderswag2.Services.DataService
@@ -11,14 +14,27 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
-
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
+/*
+            **** The following doesn't work for RecyclerView
+            * --------------------------------------------------
+            *
+        categoryListView.setOnItemClickListener { parent, view, position, id ->
+            val category = DataService.categories[position]
+            Toast.makeText(this, "You clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show()
+        }
+*/
 
     }
 }
